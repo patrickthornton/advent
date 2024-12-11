4d9a2720444e1f80bec9fc1cea60bcd72e7d80a5
patrick thornton <patrickthornton@college.harvard.edu>
2024-12-11 13:27:17 -0500

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,26 @@
-use std::{collections::HashMap, iter::zip};
+pub mod input;
+use std::{
+    collections::HashMap,
+    iter::{once, zip},
+};
 
-use anyhow::{Context, Ok, Result};
-
-fn get_two_lists(filename: &str) -> Result<(Vec<u32>, Vec<u32>)> {
-    let contents = std::fs::read_to_string(filename)?;
-    let split_lines = contents
-        .lines()
-        .map(|line| {
-            line.split("   ")
-                .map(|num| num.parse::<u32>().context("bad parse"))
-                .collect::<Result<Vec<u32>>>()
-        })
-        .collect::<Result<Vec<Vec<u32>>>>()?;
-    let (list1, list2) = split_lines
-        .iter()
-        .map(|number_list| (number_list[0], number_list[1]))
-        .unzip();
-    Ok((list1, list2))
-}
+use anyhow::{Ok, Result};
+use itertools::Itertools;
 
 // loglinear due to sorts
-fn pz1_1(input: &str) -> Result<()> {
-    let (mut list1, mut list2) = get_two_lists(input)?;
+fn pz1_1(input: &str) -> Result<u32> {
+    let (mut list1, mut list2) = input::two_lists(input)?;
     list1.sort_unstable();
     list2.sort_unstable();
 
     let result: u32 = zip(list1, list2).map(|(a, b)| a.abs_diff(b)).sum();
-    println!("{}", result);
 
-    Ok(())
+    Ok(result)
 }
 
 // amortized linear
-fn pz1_2(input: &str) -> Result<()> {
-    let (list1, list2) = get_two_lists(input)?;
+fn pz1_2(input: &str) -> Result<u32> {
+    let (list1, list2) = input::two_lists(input)?;
     let multiplicities: HashMap<u32, u32> =
         list2
             .iter()
@@ -46,14 +33,78 @@ fn pz1_2(input: &str) -> Result<()> {
         .iter()
         .map(|num| multiplicities.get(num).map_or(0, |mult| num * mult))
         .sum();
-    println!("{}", result);
 
-    Ok(())
+    Ok(result)
+}
+
+// linear
+fn pz2_1(input: &str) -> Result<usize> {
+    let mut list = input::list_per_line(input)?;
+
+    // a little strange; if first two digits are ascending,
+    // then reverse it, otherwise don't touch it; then we can
+    // treat the cases uniformly
+    for line in list.iter_mut() {
+        if line[0] < line[1] {
+            line.reverse();
+        }
+    }
+
+    let result: usize = list
+        .iter()
+        .map(|line| {
+            line.windows(2)
+                .map(|pair| pair[0] - pair[1])
+                .all(|diff| diff >= 1 && diff <= 3)
+        })
+        .filter(|&b| b)
+        .count();
+
+    Ok(result)
+}
+
+// quadratic
+// n.b.; a linear solution is very much possible, although a little painful;
+// essentially a list can permit up to two differences < 1 or > 3 next to each other
+// somewhere in the list, and the bad apple is one of the two elements in the lattermost
+// discrepant pair. so there's only two places to check for bad-apple-ness. but on
+// this data it's totally not worth it; quadratic is likely faster, even
+fn pz2_2(input: &str) -> Result<usize> {
+    let list = input::list_per_line(input)?;
+
+    fn is_valid(line: &[&u32]) -> bool {
+        line.windows(2)
+            .map(|pair| pair[0] - pair[1])
+            .all(|diff| diff >= 1 && diff <= 3)
+            || line
+                .windows(2)
+                .map(|pair| pair[1] - pair[0])
+                .all(|diff| diff >= 1 && diff <= 3)
+    }
+
+    let result: usize = list
+        .iter()
+        .map(|line| {
+            let full_line_ref = line.iter().collect::<Vec<&u32>>();
+            line.iter()
+                .combinations(line.len() - 1) // all but one
+                .chain(once(full_line_ref)) // plus the all case
+                .any(|comb| is_valid(&comb))
+        })
+        .filter(|&b| b)
+        .count();
+
+    Ok(result)
 }
 
 fn main() -> Result<()> {
-    pz1_1("input/1.txt")?;
-    pz1_2("input/1.txt")?;
+    println!("day 1, part 1: {}", pz1_1("input/1.txt")?);
+    println!("day 1, part 2: {}", pz1_2("input/1.txt")?);
+    println!();
+
+    println!("day 2, part 1: {}", pz2_1("input/2.txt")?);
+    println!("day 2, part 2: {}", pz2_2("input/2.txt")?);
+    println!();
 
     Ok(())
 }
